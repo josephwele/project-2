9888ccdf8a7f7e46fa354ab9a8651006c3b905e2
josephwele <josiwele@gmail.com>
2019-06-09 22:31:07 -0700

    userauthentication and login done

--- Unsolved/authentication.js
+++ Unsolved/authentication.js
@@ -4,24 +4,39 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('futurosenso-log');
 const user = require('futurosenso-user-mysql');
-const config = require('./config/userAuthentication');
-
+const config = require('./config/confige.js');
+console.log(config.sqlParams);
 /// / EXPRESS INIT ////
 
 var app = express();
 app.use(cors());
 app.use(bodyParser.text({ limit: '1kb' }));
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 300;
 const ENV = process.env.NODE_ENV || 'development';
+/// / DATABASE INIT ////
+if (!user.connectToDatabase(config.sqlParams)) {
+    logger.log('Error connecting to database');
+    process.exit(1);
+}
+
+user.tableExisting(config.userTableName, (error, existing) => {
+    if (!error && !existing) {
+        user.init(config.sqlParams, config.userTableName, (error, initDone) => {
+            if (error || !initDone) {
+                logger.log("Error occured during database initialization");
+                process.exit(1);
+            }
+        });
+    }
+});
 
 // USER LOGIN
-app.post('/user/login', function(req, res) {
-    console.log('someone access me');
+app.post('/user/login/:email/:pass', function(req, res) {
     let info = req.body;
-    if (user.isEmailValid(info.email)) {
-        if (user.isPasswordValid(info.pass)) {
-            user.login(info.email, info.pass, (error, isLoginCorrect, isPassCorrect, authToken) => {
+    if (user.isEmailValid(req.params.email)) {
+        if (user.isPasswordValid(req.params.pass)) {
+            user.login(req.params.email, req.params.pass, (error, isLoginCorrect, isPassCorrect, authToken) => {
                 if (error) {
                     res.json({ success: false, error: 'system error' });
                 } else if (!isLoginCorrect) {
@@ -40,37 +55,41 @@ app.post('/user/login', function(req, res) {
     }
 });
 // USER CREATION
-app.post('/user/create', function(req, res) {
-    console.log('someone access me');
+app.post('/user/create/:email/:pass', function(req, res) {
+    console.log('someone access create');
     let info = req.body;
-    if (user.isEmailValid(info.email)) {
-        user.isEmailAlreadyTaken(info.email, (error, existing) => {
+    console.log(req.params);
+    if (user.isEmailValid(req.params.email)) {
+        console.log('email is valid');
+        user.isEmailAlreadyTaken(req.params.email, (error, existing) => {
             if (error) {
                 res.json({ success: false, error: 'system error' });
             } else if (existing) {
                 res.json({ success: false, error: 'email not available' });
             } else {
-                if (user.isPasswordValid(info.pass)) {
-                    user.createUser(info.email, info.pass, info.userData ? info.userData : {},
+                if (user.isPasswordValid(req.params.pass)) {
+                    user.createUser(req.params.email, req.params.pass, info.userData ? info.userData : {},
                         (error, loginData, userData, confirmToken) => {
                             if (!error && confirmToken) {
                                 res.json({ success: true, confirmToken: confirmToken });
                             } else {
                                 res.json({ success: false, error: 'user not created' });
                             }
-                        })
+                        });
                 } else {
                     res.json({ success: false, error: 'password not valid' });
                 }
             }
-        })
+        });
     } else {
         res.json({ success: false, error: 'email not valid' });
     }
-})
+});
+// START SERVER LISTENING - PLESK SYSTEM STYLE ////
+
 app.listen(PORT, function() {
     console.log(
         '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
         PORT,
-        PORT);
+        PORT)
 })
---
+++ Unsolved/config/confige.js
@@ -0,0 +1,10 @@
+exports.sqlParams = {
+    connectionLimit: 10,
+    host: 'localhost',
+    port: '3306',
+    database: 'userTest2',
+    user: 'root',
+    password: 'enteryourpassword'
+};
+exports.userTableName = 'user';
+exports.clientAppToken = ['sdfls-fsdfl-2323D-dseer-qzlot-sgegq'];
