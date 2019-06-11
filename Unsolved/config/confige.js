bb3cbdd93426ef2fe01e24ce0f3bd69e32a3487f
josephwele <josiwele@gmail.com>
2019-06-10 23:11:30 -0700

    login and user creation done

--- Unsolved/config/confige.js
+++ Unsolved/config/confige.js
@@ -2,9 +2,9 @@ exports.sqlParams = {
     connectionLimit: 10,
     host: 'localhost',
     port: '3306',
-    database: 'userTest2',
+    database: 'newUser2',
     user: 'root',
-    password: 'Sgh19820501'
+    password: 'Gondermylife12'
 };
 exports.userTableName = 'user';
 exports.clientAppToken = ['sdfls-fsdfl-2323D-dseer-qzlot-sgegq'];
