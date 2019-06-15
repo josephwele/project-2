f536765a809b222aea557ca58bd40b9aa6f97203
josephwele <josiwele@gmail.com>
2019-06-15 09:54:15 -0700

    heroku updates

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const config = require('./configration/confige.js')
 const knex = require('./configration/connection.js')
 var favicon = require('serve-favicon')
 var path = require('path')
+const { Pool } = require('pg')
+const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    })
     // const match = require('./matcher')
     // / / EXPRESS INIT ////
 
@@ -128,6 +133,27 @@ app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, './public/index.html'))
     })
     // START SERVER LISTENING - PLESK SYSTEM STYLE ////
+app.get('/times', (req, res) => res.send(showTimes()))
+showTimes = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+        result += i + ' '
+    }
+    return result
+}
+app.get('/db', async(req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 
 app.listen(PORT, function() {
     console.log(
